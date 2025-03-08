a770979f7a921f2d6bd3ea1fd9798e844c4b3b75
Yonas <yonasdesta2012gmail.com>
2025-03-08 14:25:20 -0500

    modify app.py

--- Lang chain Project/1-Question/app.py
+++ Lang chain Project/1-Question/app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #langsmith tracking
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT", "default_value")
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING-V2"] = "true"
 
