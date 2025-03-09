5a20b96fba8801aee389e97104b708b3f726ada2
Yonas <yonasdesta2012gmail.com>
2025-03-09 10:27:50 -0400

    modify app.py

--- Lang chain Project/1-Question/app.py
+++ Lang chain Project/1-Question/app.py
@@ -23,13 +23,13 @@ os.environ["LANGCHAIN_API_KEY"] = api_key
 os.environ["LANGCHAIN_TRACING-V2"] = "true"
 
 
-prompt = ChatPromptTemplate.from_messages(
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
     [
-        ("system", "You are helpfull asistance. please responce to the user query"),
-        ("user", "Question:{question}"),
+        ("system","You are a helpful massistant . Please  repsonse to the user queries"),
+        ("user","Question:{question}")
     ]
 )
-
 
 def generate_response(question,api_key,engine,temperature,max_tokens):
     openai.api_key=api_key
@@ -40,21 +40,25 @@ def generate_response(question,api_key,engine,temperature,max_tokens):
     answer=chain.invoke({'question':question})
     return answer
 
-#Title of the App
-st.title("Question and Answer Chatbot With OpenAI")
+## #Title of the app
+st.title("Enhanced Q&A Chatbot With OpenAI")
 
-st.sidebar.title("setting")
-api_key = st.sidebar.text_input("Enter Your OpenAI API Key:" , type = "password")
 
-#engine = st.sidebar.selectbox("Select an Open AI Model", ["GPT-4 Turbo","GPT-4","GPT-4o"])
+
+## Sidebar for settings
+st.sidebar.title("Settings")
+api_key=st.sidebar.text_input("Enter your Open AI API Key:",type="password")
+
+## Select the OpenAI model
 engine=st.sidebar.selectbox("Select Open AI model",["gpt-4o","gpt-4-turbo","gpt-4"])
 
-temperature =st.sidebar.slider("Temperature" , min_value=0.0 ,max_value=1.0 ,value=0.7)
-max_tokens = st.sidebar.slider("Max Tokens" , min_value=50,max_value=300,value=150)
+## Adjust response parameter
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
 
-
-st.write("what is you question ?")
-user_input = st.text_input("You:")
+## MAin interface for user input
+st.write("Goe ahead and ask any question")
+user_input=st.text_input("You:")
 
 if user_input and api_key:
     response=generate_response(user_input,api_key,engine,temperature,max_tokens)
@@ -64,3 +68,5 @@ elif user_input:
     st.warning("Please enter the OPen AI aPi Key in the sider bar")
 else:
     st.write("Please provide the user input")
+
+
