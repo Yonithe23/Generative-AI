c0ca58c8558e335454b89d722a6c036d12dfd699
Yonas <yonasdesta2012gmail.com>
2025-03-09 10:25:40 -0400

    modify model  app.py

--- Lang chain Project/1-Question/app.py
+++ Lang chain Project/1-Question/app.py
@@ -46,7 +46,9 @@ st.title("Question and Answer Chatbot With OpenAI")
 st.sidebar.title("setting")
 api_key = st.sidebar.text_input("Enter Your OpenAI API Key:" , type = "password")
 
-engine = st.sidebar.selectbox("Select an Open AI Model", ["GPT-4 Turbo","GPT-4","GPT-4o"])
+#engine = st.sidebar.selectbox("Select an Open AI Model", ["GPT-4 Turbo","GPT-4","GPT-4o"])
+engine=st.sidebar.selectbox("Select Open AI model",["gpt-4o","gpt-4-turbo","gpt-4"])
+
 temperature =st.sidebar.slider("Temperature" , min_value=0.0 ,max_value=1.0 ,value=0.7)
 max_tokens = st.sidebar.slider("Max Tokens" , min_value=50,max_value=300,value=150)
 
